e3300bfb4e46b1eaf7713b56223f8fd7b11f31d1
Simon Lagos <simon.lagos.sallhed@gmail.com>
2023-03-23 23:23:00 +0100

    Add project outline

---
+++ index.test.ts
@@ -0,0 +1,5 @@
+import { test } from 'vitest'
+
+test('setup-ts-project', () => {
+  // TODO
+})
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,34 @@
+import chalk from 'chalk'
+import { execa } from 'execa'
+import { promises as fs } from 'fs'
 
+const templateGitignore = `node_modules
+dist
+DS_Store`
+
+console.log()
+console.log(chalk.blue(' setup-ts-project'))
+console.log()
+console.log(' please wait...')
+console.log()
+
+await execa('npx', ['npm-init-ex@latest', '--yes'])
+await execa('npx', ['setup-prettier@latest', '--yes'])
+await execa('npx', ['setup-typescript@latest', '--yes'])
+
+await fs.writeFile('./.gitignore', templateGitignore)
+
+async function createCommit() {
+  await execa('git', ['init'])
+  await execa('git', ['add', '.'])
+  await execa('git', [
+    'commit',
+    '-m',
+    'Initialize project with setup-ts-project',
+  ])
+}
+await createCommit()
+
+console.log()
+console.log(chalk.green(' project initialized!'))
+console.log()
