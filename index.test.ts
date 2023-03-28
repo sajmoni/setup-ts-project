ce7a9ac1eacfc1cd6e222ab2de751582b78ed116
Simon Lagos <simon.lagos.sallhed@gmail.com>
2023-03-28 17:41:43 +0200

    Add a very simple test

--- index.test.ts
+++ index.test.ts
@@ -1,5 +1,20 @@
-import { test } from 'vitest'
+import { execa } from 'execa'
+import { test, expect } from 'vitest'
+import { temporaryDirectory } from 'tempy'
 
-test('setup-ts-project', () => {
-  // TODO
-})
+test('setup-ts-project', async () => {
+  const directory = temporaryDirectory()
+
+  const { stdout } = await execa('setup-ts-project', [], {
+    cwd: directory,
+    env: {
+      // @ts-expect-error
+      FORCE_COLOR: 2,
+    },
+  })
+
+  console.log('stdout', stdout)
+
+  // TODO: Improve this test?
+  expect(stdout).toMatchSnapshot()
+}, 20000)
